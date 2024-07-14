6f45bdd5775ac15fe8588faa0dfc49064a9a3f1a
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-07-14 19:00:59 +0530

    prgm on 14-07

---
+++ anagram.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class anagram {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		String st1=obj.nextLine();
+		String st2=obj.nextLine();
+		st1=st1.toLowerCase(); st2=st2.toLowerCase();
+		if(st1.length()!=st2.length()) 
+			System.out.println("Not Anagram");
+		else {
+		char[] s1=st1.toCharArray();
+		char[] s2=st2.toCharArray();
+		Arrays.sort(s1);
+		Arrays.sort(s2);
+		if(Arrays.equals(s1,s2)==true)
+			System.out.println("Anagram");
+		else
+			System.out.println("Not Anagram");
+	}
+
+}
+}
