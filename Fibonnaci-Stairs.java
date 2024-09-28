acb172dedc3d2ef107355aa46223c85340bb6b45
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-09-28 12:18:03 +0530

    28/09

---
+++ Fibonnaci-Stairs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		int n=obj.nextInt();
+		int n1=0,n2=1;
+		int n3=0;
+		for(int i=0;i<n;i++){
+		    n3=n1+n2;
+		    n1=n2;
+		    n2=n3;
+		}
+		System.out.println(n3);
+	}
+}
