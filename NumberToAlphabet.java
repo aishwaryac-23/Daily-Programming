a40a844dad5817027b47b83cf733e6223fb8203a
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-09-30 16:02:45 +0530

    30

---
+++ NumberToAlphabet.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String st = obj.next();
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=0;i<st.length();i++){
+            char ch=st.charAt(i);
+            char s = (char) ('a' + (ch - '1'));
+            result.append(s);
+        }
+        
+        System.out.println(result.toString());
+    }
+}
