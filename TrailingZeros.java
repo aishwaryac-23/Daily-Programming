6c589e1c0543d0c5440cd935ef3ed74a7c1c995b
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-10-03 11:51:42 +0530

    03/10
    Example:
    Input:
    100
    Output:
    24
    Explanation:
    If Input is less than 5 it has no Trailing Zeros
    If Equal to or Greater than 5 then count the Trailing Zeroes
    Input is 5 ,5! =120 then Output is 1

---
+++ TrailingZeros.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        System.out.println(TrailingZeros(n));
+    }
+    public static int TrailingZeros(int n){
+        int sum=0;
+        if(n<5) return 0;
+        while(n>=5){
+            sum+=n/5;
+            n/=5;
+        }
+        return sum;
+    }
+}
