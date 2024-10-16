4eec66f577a5f473de1432eaa8d1368b72f7cee4
aishwaryac-23 <aishwaryac2312@gmail.com>
2024-10-16 22:52:52 +0530

    16/10

---
+++ Accenture_Program1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Accenture_Program1 {
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int k=obj.nextInt();
+        int[] ar1=new int[n];
+        int[] ar2=new int[n];
+        for(int i=0;i<n;i++){
+            ar1[i]=obj.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            ar2[i]=obj.nextInt();
+        }
+        System.out.println(MarkValidation(n,k,ar1,ar2));
+    }
+    public static String MarkValidation(int n,int k,int[] ar1,int[] ar2){
+        int sum=0;
+        Integer[] diff=new Integer[n];
+        for(int i=0;i<n;i++){
+            diff[i]=ar2[i]-ar1[i];
+        }
+        Arrays.sort(diff,Collections.reverseOrder());
+        for(int i=0;i<k;i++){
+            sum+=diff[i];
+        }
+        if(sum>=35)
+            return "Qualified "+sum;
+        else
+            return "Disqualified "+sum;
+    }
+}
