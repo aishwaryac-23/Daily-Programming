92a745537dfd0a52b271144f14f2b1b5d62f174f
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-12-19 22:10:02 +0530

    19/12

---
+++ PascalsTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i - 1; j++) {
+                System.out.print(" ");
+            }
+            int value = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(value + " ");
+                value = value * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
