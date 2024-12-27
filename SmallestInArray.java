be230f306ae7921b432ed2b6b9d145a0bbb456b8
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-12-27 23:08:51 +0530

    27/12/24

---
+++ SmallestInArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SmallestInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int smallest = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+
+        System.out.println("The smallest element in the array is: " + smallest);
+    }
+}
