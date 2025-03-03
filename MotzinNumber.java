21640d31e71ef8a7f79899cda3f0bdc9e41051cb
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-03 22:10:41 +0530

    03/03/25

---
+++ MotzinNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MotzkinNumber {
+    public static int motzkin(int n) {
+        if (n == 0 || n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (2 * i + 1) * dp[i - 1] + (3 * (i - 1)) * dp[i - 2];
+            dp[i] /= (i + 2);
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(motzkin(n));
+    }
+}
