90700c3f988f6f2b0704a63f0fc23a74bf579f2e
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-09 23:06:27 +0530

    09/03/25

---
+++ CakeNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CakeNumber {
+    public static int cakeNumber(int n) {
+        return (n * (n + 1)) / 2 + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(cakeNumber(n));
+    }
+}
