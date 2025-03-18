0570c6b39d4a50c0d8679e99604e1098473d0f5e
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-18 22:36:06 +0530

    18/03/25

---
+++ DecimalToOctal.java
@@ -0,0 +1,7 @@
+public class DecimalToOctal {
+    public static void main(String[] args) {
+        int decimal = 100;
+        System.out.println("Octal representation: " + decimalToOctal(decimal));
+    }
+
+    public static String decimalToOctal(int decimal
