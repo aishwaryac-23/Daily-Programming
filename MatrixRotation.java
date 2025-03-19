17b8f67bef96c5b399b2fa9a2ce261e1326e0b90
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-19 22:35:57 +0530

    19/03/25

---
+++ MatrixRotation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MatrixRotation {
+    public static void rotate90(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+        rotate90(matrix);
+        printMatrix(matrix);
+        sc.close();
+    }
+}
