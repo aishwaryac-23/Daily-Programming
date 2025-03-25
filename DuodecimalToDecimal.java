58390d6b0d3bca63416c09b0ab8a1272ebb3a09b
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-25 22:24:35 +0530

    25/03/25

---
+++ DuodecimalToDecimal.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DuodecimalToDecimal {
+    public static int convertToDecimal(String duodecimal) {
+        int decimal = 0, power = 0;
+
+        for (int i = duodecimal.length() - 1; i >= 0; i--) {
+            char ch = duodecimal.charAt(i);
+            int value;
+
+            if (ch >= '0' && ch <= '9')
+                value = ch - '0';
+            else if (ch == 'A')
+                value = 10;
+            else if (ch == 'B')
+                value = 11;
+            else
+                return -1;  // Invalid input
+
+            decimal += value * Math.pow(12, power);
+            power++;
+        }
+        return decimal;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a duodecimal number: ");
+        String duodecimal = scanner.next();
+        
+        int decimal = convertToDecimal(duodecimal);
+        if (decimal == -1)
+            System.out.println("Invalid input!");
+        else
+            System.out.println("Decimal: " + decimal);
+
+        scanner.close();
+    }
+}
