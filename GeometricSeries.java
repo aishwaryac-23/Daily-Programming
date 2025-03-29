22d7f5f70649f2b8c6556d41f3a40602caeebd21
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-03-29 22:49:57 +0530

    29/03/25

---
+++ GeometricSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GeometricSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first term (a): ");
+        int a = scanner.nextInt();
+        System.out.print("Enter common ratio (r): ");
+        int r = scanner.nextInt();
+        System.out.print("Enter number of terms (n): ");
+        int n = scanner.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            a *= r;
+        }
+        
+        scanner.close();
+    }
+}
