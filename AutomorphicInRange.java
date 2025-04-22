033925bc0cf506ccf0e74dc3768eabafd5edcca0
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-04-22 22:27:24 +0530

    22/04/25

---
+++ AutomorphicInRange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AutomorphicInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter lower bound: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter upper bound: ");
+        int end = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Automorphic numbers in the range:");
+        for (int i = start; i <= end; i++) {
+            if (isAutomorphic(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isAutomorphic(int num) {
+        int square = num * num;
+        String numStr = String.valueOf(num);
+        String squareStr = String.valueOf(square);
+        return squareStr.endsWith(numStr);
+    }
+}
