56d7ef5ae01c064ba5ecd57731b8973d6656c8c6
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-05-26 22:40:01 +0530

    26/05/25

---
+++ LastWordLength.java
@@ -0,0 +1,24 @@
+public class LastWordLength {
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello World";
+        System.out.println("Length of last word: " + lengthOfLastWord(input)); // Output: 5
+    }
+}
