d7fd8e068ed8ee0a1e9267875fce24c55ce8d869
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-07-04 23:26:35 +0530

    04/07/25

---
+++ ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        System.out.println("List: " + list);
+        System.out.println("First element: " + list.get(0));
+        list.remove("Banana");
+        System.out.println("After removal: " + list);
+        System.out.println("Size: " + list.size());
+    }
+}
