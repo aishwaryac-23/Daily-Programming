3d2fa3b005b7beaf5376f535836b239fc4c5f47e
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-07-26 22:52:27 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,6 +8,7 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
+        
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
