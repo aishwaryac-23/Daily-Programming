574fb31d455428338f8beb7561cf8ab87e52c753
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-07-28 22:49:07 +0530

    Update CakeNumber.java

--- CakeNumber.java
+++ CakeNumber.java
@@ -4,7 +4,6 @@ public class CakeNumber {
     public static int cakeNumber(int n) {
         return (n * (n + 1)) / 2 + 1;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
