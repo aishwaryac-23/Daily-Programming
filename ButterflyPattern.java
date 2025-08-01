d61317c533a6c9ab9730fa7ec8e2a894b52d2a7d
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-08-01 22:47:04 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -18,7 +18,8 @@ public class ButterflyPattern {
             }
             System.out.println();
         }
-
+    }
+        
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
