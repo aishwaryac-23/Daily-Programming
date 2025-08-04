272f0403f57e34004fe9f8c65abfb2e88de4087b
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-08-04 22:55:11 +0530

    Update BuzzNumber.java

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -5,7 +5,7 @@ public class BuzzNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
-        
+    
         if (number % 7 == 0 || number % 10 == 7) {
             System.out.println(number + " is a Buzz number.");
         } else {
