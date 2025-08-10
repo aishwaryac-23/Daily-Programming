f59a6ef5b114f6fb07e90720e94406cc2cc188ad
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-08-10 23:43:49 +0530

    Update AlphabetTriangle.java

--- AlphabetTriangle.java
+++ AlphabetTriangle.java
@@ -4,7 +4,6 @@ public class AlphabetTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-
         for (int i = 1; i <= rows; i++) {
             char ch = 'A';
             for (int j = 1; j <= i; j++) {
