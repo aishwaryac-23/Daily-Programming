dd8a3028a798b8c3c73b99a95f55e8e5b36978a8
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-08-13 23:46:06 +0530

    Update ArrayProgram.java

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -6,7 +6,6 @@ public class ArrayProgram {
         int n = sc.nextInt();
         int[] arr = new int[n];
         int sum = 0;
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
             sum += arr[i];
