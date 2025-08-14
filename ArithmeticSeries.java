a75a8ae5f4dbe589c7141e1e27ea1e7f40af6e85
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-08-15 00:03:52 +0530

    Update ArithmeticSeries.java

--- ArithmeticSeries.java
+++ ArithmeticSeries.java
@@ -9,7 +9,7 @@ public class ArithmeticSeries {
         int d = scanner.nextInt();
         System.out.print("Enter number of terms (n): ");
         int n = scanner.nextInt();
-        
+       
         for (int i = 0; i < n; i++) {
             System.out.print(a + " ");
             a += d;
